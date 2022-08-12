119e1ba3428e4addcb85478c15a1e890fb7cf0aa
VasckMe <kasarin.anton@gmail.com>
2022-08-12 19:48:12 +0300

    Project created

---
+++ ContactsApp/ContactsApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Apple Macbook Pro 13 on 12.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
